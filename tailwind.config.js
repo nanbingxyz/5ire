ce408bfaf2fd4832c5e597dd7ec9f24256319b3a
Angx <norex@qq.com>
2025-10-24 16:03:05 +0800

    chore(deps): upgrade tailwind css version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/!(node_modules)/**/*.{jsx,tsx}'],
+  content: ["./src/**/!(node_modules)/**/*.{jsx,tsx,scss,sass}"],
   theme: {
     extend: {},
   },
   plugins: [],
-  darkMode: "class"
+  darkMode: "class",
 };
