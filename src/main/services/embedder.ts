a97aa3e2945e009912ce8b055f8064d53d35562f
Angx <norex@qq.com>
2025-10-30 11:16:30 +0800

    refactor(embedder): restructure and optimize embedder implementation

--- src/main/services/embedder.ts
+++ src/main/services/embedder.ts
@@ -1,6 +1,6 @@
 import { join } from "node:path";
 import type { FeatureExtractionPipeline } from "@xenova/transformers/types/pipelines";
-import { move, rm, stat } from "fs-extra";
+import { ensureDir, move, rm, stat } from "fs-extra";
 import { DOCUMENT_EMBEDDING_MODEL_FILES, DOCUMENT_EMBEDDING_MODEL_NAME } from "@/main/constants";
 import { Environment } from "@/main/environment";
 import { Container } from "@/main/internal/container";
@@ -14,17 +14,25 @@ export class Embedder extends Store<Embedder.State> {
   constructor() {
     super(() => {
       return {
-        status: "idle",
+        status: {
+          type: "idle",
+        },
+        model: DOCUMENT_EMBEDDING_MODEL_NAME,
+        files: DOCUMENT_EMBEDDING_MODEL_FILES.map((file) => file.name),
       };
     });
   }
 
   async init() {
-    if (this.state.status !== "idle") {
+    if (this.state.status.type !== "idle") {
       return;
     }
 
-    this.replace({ status: "initializing" });
+    this.update((draft) => {
+      draft.status = {
+        type: "initializing",
+      };
+    });
 
     let missing = false;
     let partially = false;
@@ -45,7 +53,12 @@ export class Embedder extends Store<Embedder.State> {
     }
 
     if (missing) {
-      return this.replace({ status: "unavailable", reason: partially ? "model-partially-missing" : "model-missing" });
+      return this.update((draft) => {
+        draft.status = {
+          type: "unavailable",
+          reason: partially ? "model-partially-missing" : "model-missing",
+        };
+      });
     }
 
     const { env, pipeline } = await import("@xenova/transformers");
@@ -59,38 +72,43 @@ export class Embedder extends Store<Embedder.State> {
 
     await pipeline("feature-extraction", DOCUMENT_EMBEDDING_MODEL_NAME)
       .then((extractor) => {
-        this.replace({ status: "ready", extractor, running: 0 });
+        this.update((draft) => {
+          draft.status = { type: "ready", extractor, running: 0 };
+        });
       })
       .catch(() => {
-        this.replace({ status: "unavailable", reason: "pipeline-init-failed" });
+        this.update((draft) => {
+          draft.status = { type: "unavailable", reason: "pipeline-init-failed" };
+        });
       });
   }
 
-  async remove() {
-    if (this.state.status !== "unavailable" && this.state.status !== "ready") {
+  async removeModel() {
+    if (this.state.status.type !== "unavailable" && this.state.status.type !== "ready") {
       throw new Error("Embedder is not unavailable or ready");
     }
 
-    if (this.state.status === "ready") {
-      await this.state.extractor.dispose();
+    if (this.state.status.type === "ready") {
+      await this.state.status.extractor.dispose().catch(() => {});
     }
 
-    await rm(this.#environment.embedderModelsFolder, { recursive: true });
+    await rm(this.#environment.embedderModelsFolder, { recursive: true }).catch(() => {});
 
-    this.replace({
-      status: "unavailable",
-      reason: "model-missing",
+    this.update((draft) => {
+      draft.status = { type: "unavailable", reason: "model-missing" };
     });
   }
 
-  async download(signal: AbortSignal, onProgress?: (name: string, received: number, total: number) => void) {
-    if (this.state.status !== "unavailable") {
+  async downloadModel() {
+    if (this.state.status.type !== "unavailable") {
       throw new Error("Embedder is not unavailable");
     }
 
-    await rm(this.#environment.embedderModelsFolder, { recursive: true });
+    await rm(this.#environment.embedderModelsFolder, { recursive: true, force: true });
+    await ensureDir(this.#environment.embedderModelsFolder);
 
     const progress: Record<string, Record<"total" | "received", number>> = {};
+    const controller = new AbortController();
 
     for (const { name } of DOCUMENT_EMBEDDING_MODEL_FILES) {
       progress[name] = {
@@ -99,25 +117,27 @@ export class Embedder extends Store<Embedder.State> {
       };
     }
 
-    this.replace({
-      status: "downloading",
-      progress,
+    this.update((draft) => {
+      draft.status = {
+        type: "downloading",
+        progress,
+        controller,
+      };
     });
 
-    await Promise.all(
+    Promise.all(
       DOCUMENT_EMBEDDING_MODEL_FILES.map(async (file) => {
         return this.#downloader
           .download({
-            signal,
+            signal: controller.signal,
             url: file.url,
             onProgress: (received, total) => {
               this.update((draft) => {
-                if (draft.status === "downloading") {
-                  draft.progress[file.name].total = total;
-                  draft.progress[file.name].received = received;
+                if (draft.status.type === "downloading") {
+                  draft.status.progress[file.name].total = total;
+                  draft.status.progress[file.name].received = received;
                 }
               });
-              onProgress?.(file.name, received, total);
             },
           })
           .then((downloaded) => {
@@ -141,36 +161,55 @@ export class Embedder extends Store<Embedder.State> {
         );
       })
       .then(() => {
-        this.replace({ status: "idle" });
+        this.update((draft) => {
+          draft.status = {
+            type: "idle",
+          };
+        });
         this.init().catch(() => {});
       })
       .catch((e) => {
-        this.replace({ status: "unavailable", reason: "model-missing" });
+        this.update((draft) => {
+          draft.status = {
+            type: "unavailable",
+            reason: "model-missing",
+          };
+        });
 
-        throw e;
+        if (controller.signal.aborted) {
+          return;
+        }
       });
   }
 
+  async cancelDownloadModel() {
+    if (this.state.status.type !== "downloading") {
+      throw new Error("Embedder is not downloading");
+    }
+
+    this.state.status.controller.abort();
+  }
+
   async embed(text: string) {
-    if (this.state.status !== "ready") {
+    if (this.state.status.type !== "ready") {
       throw new Error("Embedder is not ready");
     }
 
     this.update((draft) => {
-      if (draft.status === "ready") {
-        draft.running++;
+      if (draft.status.type === "ready") {
+        draft.status.running++;
       }
     });
 
-    return this.state
+    return this.state.status
       .extractor(text, { pooling: "mean", normalize: true })
       .then((tersor) => {
         return tersor.tolist() as number[];
       })
       .finally(() => {
         this.update((draft) => {
-          if (draft.status === "ready") {
-            draft.running--;
+          if (draft.status.type === "ready") {
+            draft.status.running--;
           }
         });
       });
@@ -178,24 +217,31 @@ export class Embedder extends Store<Embedder.State> {
 }
 
 export namespace Embedder {
-  export type State =
+  export type Status =
     | {
-        status: "idle";
+        type: "idle";
       }
     | {
-        status: "initializing";
+        type: "initializing";
       }
     | {
-        status: "ready";
+        type: "ready";
         extractor: FeatureExtractionPipeline;
         running: number;
       }
     | {
-        status: "unavailable";
+        type: "unavailable";
         reason: "model-missing" | "model-partially-missing" | "pipeline-init-failed";
       }
     | {
-        status: "downloading";
+        type: "downloading";
         progress: Record<string, Record<"total" | "received", number>>;
+        controller: AbortController;
       };
+
+  export type State = {
+    status: Status;
+    model: string;
+    files: string[];
+  };
 }
